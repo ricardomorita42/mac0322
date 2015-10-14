ac6d651464856983a0c25ed7a1d2b8e576496a12
ricardo.morita@gmail.com <ricardo.morita@gmail.com>
2015-10-14 12:49:44 -0300

    Euler 1a versao do mov. circular implementado (ainda errado

--- ep2/lib/calc_circular.py
+++ ep2/lib/calc_circular.py
@@ -14,17 +14,20 @@
 #
 #   Referencias:
 #       https://en.wikipedia.org/wiki/Circular_motion
+#       https://en.wikipedia.org/wiki/Centripetal_force
 
 import math
 
 def simula(lista_pontos, dados_experimento, arquivo_saida):
     arq_analitico = arquivo_saida + "_analitico.csv"
+    arq_euler = arquivo_saida + "_euler.csv"
 
     simula_analitico(lista_pontos, dados_experimento, arq_analitico)
-
+    simula_euler(lista_pontos, dados_experimento,arq_euler)
 
 
 def simula_analitico(lista_pontos, dados_experimento,arquivo_saida):
+
     #dados do experimento contêm raio e número de voltas dadas.
     raio = dados_experimento[0]
     voltas = dados_experimento[1]
@@ -39,10 +42,9 @@ def simula_analitico(lista_pontos, dados_experimento,arquivo_saida):
     f.write("Instante(s),Pos_x(m),Pos_y(m)\n")
 
     for ponto_atual in lista_pontos:
-        teta = vel_ang * ponto_atual            #calculado em radianos
-        teta_em_graus = 180 * teta / math.pi
-        x = raio * math.cos(teta_em_graus)
-        y = raio * math.sin(teta_em_graus)
+        teta = vel_ang * ponto_atual
+        x = raio * math.cos(teta)
+        y = raio * math.sin(teta)
 
         f.write(str(round(ponto_atual,3)) + ',' +
                 str(round(x,3)) + ',' +
@@ -52,3 +54,74 @@ def simula_analitico(lista_pontos, dados_experimento,arquivo_saida):
 
     print "Arquivo %s criado.\n\n" %arquivo_saida
 
+def simula_euler(lista_pontos, dados_experimento,arquivo_saida):
+    #dados do experimento contêm raio e número de voltas dadas.
+    raio = dados_experimento[0]
+    voltas = dados_experimento[1]
+
+    tempo = lista_pontos[-1]            #Tempo total do experimento
+    periodo = tempo / voltas            #Tempo de uma volta
+    vel_ang = 2 * math.pi / periodo     #Velocidade Angular
+
+    print "Preparando %s..." %arquivo_saida
+
+    f = open(arquivo_saida,'w')
+    f.write("Instante(s),Pos_x(m),Pos_y(m)\n")
+
+    #Calculando Velocidade e Aceleração atual para começar a usar Euler
+    t_atual = lista_pontos.pop(0)
+    teta = vel_ang * t_atual
+    x_atual = raio * math.cos(teta)
+    y_atual = raio * math.sin(teta)
+
+    vel_x_atual = -1 * raio * vel_ang * math.sin(vel_ang*t_atual)
+    vel_y_atual =      raio * vel_ang * math.cos(vel_ang*t_atual)
+
+    #   A formula da aceleracao não é linear. Entretanto, para valores
+    #pequenos de teta, estamos considerando sen(teta) = teta,
+    #tornando esta equacao linear.
+    acel_x_atual = -1 * vel_ang ** 2 * raio * teta
+    acel_y_atual =  raio * teta
+
+    f.write(str(round(t_atual,3)) + ',' +
+            str(round(x_atual,3)) + ',' +
+            str(round(y_atual,3)) + '\n')
+
+    #Renomeando as variaveis para ficar mais claro no loop
+    t_anterior = t_atual
+    vel_x_anterior = vel_x_atual
+    vel_y_anterior = vel_y_atual
+    acel_x_anterior = acel_x_atual
+    acel_y_anterior = acel_y_atual
+    x_anterior = x_atual
+    y_anterior = y_atual
+
+    for ponto_atual in lista_pontos:
+        teta = vel_ang * ponto_atual
+        delta_t = ponto_atual - t_anterior
+
+        x_atual = x_anterior + vel_x_anterior * delta_t
+        y_atual = y_anterior + vel_y_anterior * delta_t
+
+        vel_x_atual = vel_x_anterior + acel_x_anterior*delta_t
+        vel_y_atual = vel_y_anterior + acel_y_anterior*delta_t
+
+        acel_x_atual = -1 * vel_ang ** 2 * raio * teta
+        acel_y_atual =  raio * teta
+
+        f.write(str(round(ponto_atual,3)) + ',' +
+                str(round(x_atual,3)) + ',' +
+                str(round(y_atual,3)) + '\n')
+
+        #Renomeando as variaveis
+        t_anterior = ponto_atual
+        vel_x_anterior = vel_x_atual
+        vel_y_anterior = vel_y_atual
+        acel_x_anterior = acel_x_atual
+        acel_y_anterior = acel_y_atual
+        x_anterior = x_atual
+        y_anterior = y_atual
+
+    f.close()
+
+    print "Arquivo %s criado.\n\n" %arquivo_saida
