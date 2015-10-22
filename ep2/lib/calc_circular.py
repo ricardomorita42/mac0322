ee741af68d9c21927bacec18f5ad673b64e7b442
ricardo.morita@gmail.com <ricardo.morita@gmail.com>
2015-10-22 11:32:38 -0200

    Algoritmo do mov circular corrigido

--- ep2/lib/calc_circular.py
+++ ep2/lib/calc_circular.py
@@ -71,24 +71,11 @@ def simula_euler(lista_pontos, dados_experimento,arquivo_saida):
     f = open(arquivo_saida,'w')
     f.write("Instante(s),Pos_x(m),Pos_y(m)\n")
 
-    #Calculando Velocidade e Aceleração atual para começar a usar Euler
+    #Calculando Velocidade
     t_atual = lista_pontos.pop(0)
-    teta = vel_ang * t_atual
-    x_atual = raio * math.cos(teta)
-    y_atual = raio * math.sin(teta)
-
-    vel_x_atual = -1 * raio * vel_ang * math.sin(vel_ang*t_atual)
-    vel_y_atual =      raio * vel_ang * math.cos(vel_ang*t_atual)
-
-    #   A formula da aceleracao não é linear. Entretanto, para valores
-    #pequenos de teta, estamos considerando:
-    #
-    #   sen(teta) = teta
-    #   cos(teta) = 1 - teta**2/2
-    #
-
-    acel_x_atual = -1 * vel_ang ** 2 * raio * (1 - teta**2/2)
-    acel_y_atual = -1 * vel_ang ** 2 * raio * teta
+    teta_atual = vel_ang * t_atual
+    x_atual = raio * math.cos(teta_atual)
+    y_atual = raio * math.sin(teta_atual)
 
     f.write(str(round(t_atual,3)) + ',' +
             str(round(x_atual,3)) + ',' +
@@ -96,25 +83,18 @@ def simula_euler(lista_pontos, dados_experimento,arquivo_saida):
 
     #Renomeando as variaveis para ficar mais claro no loop
     t_anterior = t_atual
-    vel_x_anterior = vel_x_atual
-    vel_y_anterior = vel_y_atual
-    acel_x_anterior = acel_x_atual
-    acel_y_anterior = acel_y_atual
-    x_anterior = x_atual
-    y_anterior = y_atual
+    teta_anterior = teta_atual
 
     for ponto_atual in lista_pontos:
         delta_t = ponto_atual - t_anterior
-        teta = vel_ang * delta_t
 
-        x_atual = x_anterior + vel_x_anterior * delta_t
-        y_atual = y_anterior + vel_y_anterior * delta_t
+        #   Como é MCU, não há Acel. Angular e portanto vel.
+        #angular é constante no algoritmo de Euler
 
-        vel_x_atual = vel_x_anterior + acel_x_anterior*delta_t
-        vel_y_atual = vel_y_anterior + acel_y_anterior*delta_t
+        teta_atual = teta_anterior + vel_ang * delta_t
 
-        acel_x_atual = -1 * vel_ang ** 2 * raio * (1 - teta**2/2)
-        acel_y_atual = -1 * vel_ang ** 2 * raio * teta
+        x_atual = raio * math.cos(teta_atual)
+        y_atual = raio * math.sin(teta_atual)
 
         f.write(str(round(ponto_atual,3)) + ',' +
                 str(round(x_atual,3)) + ',' +
@@ -122,17 +102,14 @@ def simula_euler(lista_pontos, dados_experimento,arquivo_saida):
 
         #Renomeando as variaveis
         t_anterior = ponto_atual
-        vel_x_anterior = vel_x_atual
-        vel_y_anterior = vel_y_atual
-        acel_x_anterior = acel_x_atual
-        acel_y_anterior = acel_y_atual
-        x_anterior = x_atual
-        y_anterior = y_atual
+        teta_anterior = teta_atual
 
     f.close()
 
     print "Arquivo %s criado.\n\n" %arquivo_saida
 
+#Obs.: Como a velocidade angular é constante, o algoritmo de Euler-Cromer
+#dá o mesmo resultado do que o de Euler.
 def simula_euler_cromer(lista_pontos, dados_experimento,arquivo_saida):
     #dados do experimento contêm raio e número de voltas dadas.
     raio = dados_experimento[0]
@@ -147,24 +124,11 @@ def simula_euler_cromer(lista_pontos, dados_experimento,arquivo_saida):
     f = open(arquivo_saida,'w')
     f.write("Instante(s),Pos_x(m),Pos_y(m)\n")
 
-    #Calculando Velocidade e Aceleração atual para começar a usar Euler
+    #Calculando Velocidade
     t_atual = lista_pontos.pop(0)
-    teta = vel_ang * t_atual
-    x_atual = raio * math.cos(teta)
-    y_atual = raio * math.sin(teta)
-
-    vel_x_atual = -1 * raio * vel_ang * math.sin(vel_ang*t_atual)
-    vel_y_atual =      raio * vel_ang * math.cos(vel_ang*t_atual)
-
-    #   A formula da aceleracao não é linear. Entretanto, para valores
-    #pequenos de teta, estamos considerando:
-    #
-    #   sen(teta) = teta
-    #   cos(teta) = 1 - teta**2/2
-    #
-
-    acel_x_atual = -1 * vel_ang ** 2 * raio * (1 - teta**2/2)
-    acel_y_atual = -1 * vel_ang ** 2 * raio * teta
+    teta_atual = vel_ang * t_atual
+    x_atual = raio * math.cos(teta_atual)
+    y_atual = raio * math.sin(teta_atual)
 
     f.write(str(round(t_atual,3)) + ',' +
             str(round(x_atual,3)) + ',' +
@@ -172,26 +136,18 @@ def simula_euler_cromer(lista_pontos, dados_experimento,arquivo_saida):
 
     #Renomeando as variaveis para ficar mais claro no loop
     t_anterior = t_atual
-    vel_x_anterior = vel_x_atual
-    vel_y_anterior = vel_y_atual
-    acel_x_anterior = acel_x_atual
-    acel_y_anterior = acel_y_atual
-    x_anterior = x_atual
-    y_anterior = y_atual
+    teta_anterior = teta_atual
 
     for ponto_atual in lista_pontos:
         delta_t = ponto_atual - t_anterior
-        teta = vel_ang * delta_t
 
-        vel_x_atual = vel_x_anterior + acel_x_anterior*delta_t
-        vel_y_atual = vel_y_anterior + acel_y_anterior*delta_t
+        #   Como é MCU, não há Acel. Angular e portanto vel.
+        #angular é constante no algoritmo de Euler
 
-        x_atual = x_anterior + vel_x_atual* delta_t
-        y_atual = y_anterior + vel_y_atual* delta_t
+        teta_atual = teta_anterior + vel_ang * delta_t
 
-
-        acel_x_atual = -1 * vel_ang ** 2 * raio * (1 - teta**2/2)
-        acel_y_atual = -1 * vel_ang ** 2 * raio * teta
+        x_atual = raio * math.cos(teta_atual)
+        y_atual = raio * math.sin(teta_atual)
 
         f.write(str(round(ponto_atual,3)) + ',' +
                 str(round(x_atual,3)) + ',' +
@@ -199,12 +155,7 @@ def simula_euler_cromer(lista_pontos, dados_experimento,arquivo_saida):
 
         #Renomeando as variaveis
         t_anterior = ponto_atual
-        vel_x_anterior = vel_x_atual
-        vel_y_anterior = vel_y_atual
-        acel_x_anterior = acel_x_atual
-        acel_y_anterior = acel_y_atual
-        x_anterior = x_atual
-        y_anterior = y_atual
+        teta_anterior = teta_atual
 
     f.close()
 
