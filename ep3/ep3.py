8370842b8650be6efd8836b9cab0c15253590c43
ricardo.morita@gmail.com <ricardo.morita@gmail.com>
2015-12-05 15:47:33 -0200

    adicionando comentários

--- ep3/ep3.py
+++ ep3/ep3.py
@@ -12,6 +12,7 @@
 #rodar easy_install prettytable no terminal para instalar esta lib
 from prettytable import PrettyTable
 
+#Cada vez que é chamada, devolve um dos vizinhos
 def neighbors(cell):
     x, y = cell
     yield x - 1, y - 1
@@ -23,13 +24,20 @@ def neighbors(cell):
     yield x    , y + 1
     yield x + 1, y + 1
 
+#Cada chamada itera em um passo o board
 def apply_iteration(board):
+
     new_board = set([])
+
+    #analisa os candidatos a serem povoados
     candidates = board.union(set(n for cell in board for n in neighbors(cell)))
+
+    #notar que a morte celular esta sendo levada em consideracao aqui
     for cell in candidates:
         count = sum((n in board) for n in neighbors(cell))
         if count == 3 or (count == 2 and cell in board):
             new_board.add(cell)
+
     return new_board
 
 #pega o set e desenha num table
@@ -37,17 +45,21 @@ def draw(board):
     max_row = 0
     max_col = 0
 
+    #determina o tamanho da board
     for cell in board:
         if cell[0] > max_row:
             max_row = cell[0]
         if cell[1] > max_col:
             max_col = cell[1]
 
+    #cria um array de 0s com tamanho [max_col+1,max_row+1]
     drawn_board = [[0 for col in range(max_col+1)] for row in range(max_row+1)]
 
+    #Se tiver uma celular, vira 1
     for cell in board:
         drawn_board[cell[0]][cell[1]] = 1
 
+    #desenha bonitinho
     p = PrettyTable()
     for row in drawn_board:
         p.add_row(row)
